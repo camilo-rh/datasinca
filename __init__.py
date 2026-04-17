c91a47a3c0e9ec3eea05611aad6ff330bd426418
CamiloRH <65565323+camilo-rh@users.noreply.github.com>
2026-04-17 14:52:06 -0400

    Initial version of datasinca (legacy implementation)

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 00:32:11 2024
+
+@author: Camilo
+"""
+
+from .datasinca3 import datasinca
+from ._info import Info
+info = Info()
+datasinca.info = info
